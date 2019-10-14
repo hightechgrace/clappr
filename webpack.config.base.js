6177358ff178ed6d391c2f34fd43122de2d52be3
Bruno Torres <me@brunotorr.es>
2019-10-14 16:13:51 -0300

    refactor: replace code with webpack bundles/entries importing from individual packages

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -14,29 +14,41 @@ const webpackConfig = (config) => {
       host: '0.0.0.0',
       port: 8080
     },
-    mode: config.mode,
+    mode: config.mode || 'development',
     devtool: config.devtool || 'source-maps',
     optimization: config.optimization,
-    entry: path.resolve(__dirname, 'src/main.js'),
+    entry: config.entry || path.resolve(__dirname, 'src/main.js'),
     externals: config.externals,
     module: {
       rules: [
         {
           test: /\.js$/,
           loader: 'babel-loader',
-          exclude: [path.resolve(__dirname, './node_modules')]
+          include: /node_modules\/@clappr/,
+          exclude: [
+            path.resolve(__dirname, 'node_modules', 'hls.js'),
+          ]
         },
         {
           test: /fonts\.css$/,
           loaders: ['css-loader', 'postcss-loader'],
-          include: path.resolve(__dirname, 'src/components/core/public')
+          include: [
+            path.resolve(__dirname, 'node_modules', '@clappr/plugins/src/public'),
+          ]
         },
         {
           test: /\.scss$/,
-          loaders: ['style-loader?singleton=true', 'css-loader', 'postcss-loader', 'sass-loader?includePaths[]='
-            + path.resolve(__dirname, './src/base/scss')
+          loaders: ['style-loader?singleton=true', 'css-loader', 'postcss-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                includePaths: [
+                  path.resolve(__dirname, 'node_modules', '@clappr/plugins/src/public'),
+                  path.resolve(__dirname, 'node_modules', '@clappr/plugins/src/public/scss')
+                ]
+              }
+            }
           ],
-          include: path.resolve(__dirname, 'src')
         },
         {
           test: /\.(png|woff|eot|swf|cur|ttf)/,
@@ -57,19 +69,27 @@ const webpackConfig = (config) => {
     },
     resolve: {
       alias: {
-        'clappr-zepto': 'clappr-zepto/zepto.js'
+        'clappr-zepto': 'clappr-zepto/zepto.js',
+        '@clappr/core': '@clappr/core/src/main.js',
+        '@clappr/plugins': '@clappr/plugins/src/main.js',
+        '@clappr/hlsjs-playback': '@clappr/hlsjs-playback/src/hls.js',
+        '@clappr/flash-playback': '@clappr/flash-playback/src/main.js',
+        '@clappr/flashls-playback': '@clappr/flashls-playback/src/flashls.js',
       },
       plugins: [
         new DirectoryNamedWebpackPlugin(true),
       ],
-      modules: ['node_modules']
+      modules: [
+        'node_modules'
+      ]
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
       publicPath: 'dist/',
       filename: config.filename,
       library: 'Clappr',
-      libraryTarget: 'umd'
+      libraryTarget: 'umd',
+      libraryExport: 'default',
     },
     plugins: [
       new webpack.DefinePlugin({
